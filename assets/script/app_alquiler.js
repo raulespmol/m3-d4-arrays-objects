fe3cdf1a64e32febd1692fae3fa9dfde080368b8
Raul Espinoza M <raul.espmol@gmail.com>
2023-12-09 12:37:37 -0300

    funciones para rendereizar cards

--- assets/script/app_alquiler.js
+++ assets/script/app_alquiler.js
@@ -3,36 +3,14 @@ const container = document.querySelector('#alquiler .row');
 let cards = '';
 
 propiedadesAlquiler.forEach(prop => {
-  let petHTML = '';
-  let smokeHTML = '';
+  renderCards(prop)
+})
 
-  if (prop.pets){
-    petHTML = `
-    <p class="text-success">
-    <i class="fas fa-paw"></i> Mascotas permitidas
-    </p>
-    `
-  } else {
-    petHTML = `
-    <p class="text-danger">
-    <i class="fa-solid fa-ban"></i> No se permiten mascotas
-    </p>
-    `
-  }
+container.innerHTML = cards
 
-  if (prop.smoke){
-    smokeHTML = `
-    <p class="text-success">
-    <i class="fas fa-smoking"></i> Permitido fumar
-    </p>
-    `
-  } else {
-    smokeHTML = `
-    <p class="text-danger">
-    <i class="fas fa-smoking-ban"></i> No se permite fumar
-    </p>
-    `
-  }
+function renderCards(prop){
+  let petHTML = checkPets(prop);
+  let smokeHTML = checkSmoke(prop);
   
   cards += `
   <div class="col-md-4 mb-4">
@@ -43,15 +21,9 @@ propiedadesAlquiler.forEach(prop => {
         alt="${prop.nombre}"
       />
       <div class="card-body">
-        <h5 class="card-title">
-          ${prop.nombre}
-        </h5>
-        <p class="card-text">
-          ${prop.descripcion}
-        </p>
-        <p>
-          <i class="fas fa-map-marker-alt"></i> ${prop.ubicacion}
-        </p>
+        <h5 class="card-title">${prop.nombre}</h5>
+        <p class="card-text">${prop.descripcion}</p>
+        <p><i class="fas fa-map-marker-alt"></i> ${prop.ubicacion}</p>
         <p>
           <i class="fas fa-bed"></i> ${prop.habitaciones} Habitaciones |
           <i class="fas fa-bath"></i> ${prop.banos} Baños
@@ -63,6 +35,36 @@ propiedadesAlquiler.forEach(prop => {
     </div>
   </div>
   `
-})
+}
 
-container.innerHTML = cards
+function checkPets(propiedad){
+  if (propiedad.pets){
+    return `
+    <p class="text-success">
+    <i class="fas fa-paw"></i> Mascotas permitidas
+    </p>
+    `
+  } else {
+    return `
+    <p class="text-danger">
+    <i class="fa-solid fa-ban"></i> No se permiten mascotas
+    </p>
+    `
+  }
+}
+
+function checkSmoke(propiedad){
+  if (propiedad.smoke){
+    return `
+    <p class="text-success">
+    <i class="fas fa-smoking"></i> Permitido fumar
+    </p>
+    `
+  } else {
+    return `
+    <p class="text-danger">
+    <i class="fas fa-smoking-ban"></i> No se permite fumar
+    </p>
+    `
+  }
+}
