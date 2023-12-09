0b7c13cb9f56eaf555459886cf1599a0c256664c
Raul Espinoza M <raul.espmol@gmail.com>
2023-12-09 16:00:05 -0300

    renderizar primeros 3 objetos

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,102 @@
-console.log(propiedadesAlquiler)
+const ventas = document.querySelector('#venta .row');
+const alquileres = document.querySelector('#alquiler .row');
+
+let cardsVentas = '', cardsAlquileres = ''
+
+for(let i = 0; i < 3; i++){
+  renderVenta(propiedadesVenta[i])
+  renderAlquiler(propiedadesAlquiler[i])
+}
+
+function renderVenta(prop){
+  let petHTML = checkPets(prop);
+  let smokeHTML = checkSmoke(prop);
+  
+  cardsVentas += `
+  <div class="col-md-4 mb-4">
+    <div class="card">
+      <img
+        src="./assets/img/alquiler_1.jpg"
+        class="card-img-top"
+        alt="${prop.nombre}"
+      />
+      <div class="card-body">
+        <h5 class="card-title">${prop.nombre}</h5>
+        <p class="card-text">${prop.descripcion}</p>
+        <p><i class="fas fa-map-marker-alt"></i> ${prop.ubicacion}</p>
+        <p>
+          <i class="fas fa-bed"></i> ${prop.habitaciones} Habitaciones |
+          <i class="fas fa-bath"></i> ${prop.banos} Baños
+        </p>
+        <p><i class="fas fa-dollar-sign"></i> ${prop.costo}</p>
+        ${smokeHTML}
+        ${petHTML}
+      </div>
+    </div>
+  </div>
+  `
+  ventas.innerHTML = cardsVentas
+}
+
+function renderAlquiler(prop){
+  let petHTML = checkPets(prop);
+  let smokeHTML = checkSmoke(prop);
+  
+  cardsAlquileres += `
+  <div class="col-md-4 mb-4">
+    <div class="card">
+      <img
+        src="./assets/img/alquiler_1.jpg"
+        class="card-img-top"
+        alt="${prop.nombre}"
+      />
+      <div class="card-body">
+        <h5 class="card-title">${prop.nombre}</h5>
+        <p class="card-text">${prop.descripcion}</p>
+        <p><i class="fas fa-map-marker-alt"></i> ${prop.ubicacion}</p>
+        <p>
+          <i class="fas fa-bed"></i> ${prop.habitaciones} Habitaciones |
+          <i class="fas fa-bath"></i> ${prop.banos} Baños
+        </p>
+        <p><i class="fas fa-dollar-sign"></i> ${prop.costo}</p>
+        ${smokeHTML}
+        ${petHTML}
+      </div>
+    </div>
+  </div>
+  `
+
+  alquileres.innerHTML = cardsAlquileres
+}
+
+function checkPets(propiedad){
+  if (propiedad.pets){
+    return `
+    <p class="text-success">
+    <i class="fas fa-paw"></i> Mascotas permitidas
+    </p>
+    `
+  } else {
+    return `
+    <p class="text-danger">
+    <i class="fa-solid fa-ban"></i> No se permiten mascotas
+    </p>
+    `
+  }
+}
+
+function checkSmoke(propiedad){
+  if (propiedad.smoke){
+    return `
+    <p class="text-success">
+    <i class="fas fa-smoking"></i> Permitido fumar
+    </p>
+    `
+  } else {
+    return `
+    <p class="text-danger">
+    <i class="fas fa-smoking-ban"></i> No se permite fumar
+    </p>
+    `
+  }
+}
---
+++ assets/script/prop_venta.js
@@ -0,0 +1,74 @@
+const propiedadesVenta = [
+  {
+    nombre: 'Loft con vistas panorámicas al río',
+    src: '',
+    descripcion: 'Ofrece un espacio amplio y abierto con impresionantes vistas panorámicas al río.',
+    ubicacion: '210 Magnolia Lane, Harmony Haven, CA 64208',
+    habitaciones: 2,
+    banos: 2,
+    costo: 3100,
+    smoke: false,
+    pets: true
+  },
+  
+  {
+  nombre: 'Apartamento luminoso con vista al mar',
+  src: '',
+  descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar.',
+  ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+  habitaciones: 3,
+  banos: 2,
+  costo: 2500,
+  smoke: true,
+  pets: false
+  },
+  
+  {
+  nombre: 'Condominio moderno en zona residencial',
+  src: '',
+  descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial.',
+  ubicacion: '456 Elm Street, Harmony City, CA 54321',
+  habitaciones: 3,
+  banos: 2,
+  costo: 2200,
+  smoke: false,
+  pets: true
+  },
+  
+  {
+  nombre: 'Apartamento en el centro histórico de la ciudad',
+  src: '',
+  descripcion: 'Este apartamento de estilo clásico te ofrece una experiencia única en el corazón histórico de la ciudad.',
+  ubicacion: '789 Maple Lane, Serenity Springs, CA 13579',
+  habitaciones: 2,
+  banos: 1,
+  costo: 2800,
+  smoke: false,
+  pets: false
+  },
+
+  {
+    nombre: 'Apartamento en el centro de la ciudad',
+    src: '',
+    descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+    ubicacion: '123 Main Street, Anytown, CA 91234',
+    habitaciones: 2,
+    banos: 2,
+    costo: 2000,
+    smoke: false,
+    pets: true
+  },
+  
+  {
+  nombre: 'Casa de campo con jardín y piscina privada',
+  src: '',
+  descripcion: 'Un refugio perfecto para aquellos que buscan paz y comodidad.',
+  ubicacion: '234 Redwood Drive, Whispering Pines, CA 75309',
+  habitaciones: 4,
+  banos: 3,
+  costo: 2700,
+  smoke: true,
+  pets: true
+  },
+  
+  ]
